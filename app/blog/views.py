823a7351232942f48de94976d416f0308875bfc7
kingkk <917536172@qq.com>
2018-06-21 11:09:38 +0800

    完成大部分前台模块

--- app/blog/views.py
+++ app/blog/views.py
@@ -2,7 +2,7 @@
 # @Author: King kaki
 # @Date:   2018-06-15 10:07:47
 # @Last Modified by:   King kaki
-# @Last Modified time: 2018-06-18 16:24:18
+# @Last Modified time: 2018-06-21 11:06:15
 from . import blog
 from flask import render_template, redirect, url_for, session, flash, request
 from app.models import *
@@ -12,12 +12,17 @@ def index():
 	articles = Article.query.all()
 	return render_template('blog/index.html', articles=articles)
 
+
 @blog.route('/article/<int:id>')
 def article(id):
 	article = Article.query.get(id)
 	author = Member.query.get(article.user_id)
+
+	click = Article.query.filter_by(id=id).update(dict(click_num = article.click_num+1))
+	db.session.commit()
 	comments = Comment.query.filter_by(article_id=id).all()
 	comments_author=[]
 	for comment in comments:
 		comments_author.append(Member.query.get(comment.user_id))
 	return render_template('blog/article.html', article=article, author=author, comments=comments, comments_author=comments_author)
+
